1d8719fac87a65d61fa63ce0ebedb28f432b446b
Saleh Shamloo <SLHHero@gmail.com>
2021-12-08 21:06:22 +0330

    removed temporary files

--- ps8-ising-model/bench.jl
+++
@@ -1,7 +0,0 @@
-cd(dirname(@__FILE__))
-include("Ising.jl")
-using BenchmarkTools
-isingmodel = Ising.IsingModel(1, 64, 64)
-# @benchmark Ising.update!(isingmodel)
-Ising.simulate!(isingmodel, 1000)
-@benchmark Ising.corlen(isingmodel)
